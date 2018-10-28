9a105f5421e8081b78c24bb893af5130a6cb2de4
Dean Radcliffe <deanmisc@yahoo.com>
2018-10-28 11:14:26 -0500

    First client-server connected Hotel California
    What's the Storybook?

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
-    response: ''
+    response: ""
   };
 
   componentDidMount() {
@@ -16,7 +16,7 @@ class App extends Component {
   }
 
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch("/api/hello");
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to The Hotel California</h1>
         </header>
         <p className="App-intro">{this.state.response}</p>
       </div>
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3120;
+
+app.use(morgan("dev"));
 
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get("/api/hello", (req, res) => {
+  res.send({ express: "Hello From The Server." });
 });
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
