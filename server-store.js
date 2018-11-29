daf77b7eba483a9bb5527e0e8eed65ff6f90cb04
Dean Radcliffe <deanmisc@yahoo.com>
2018-11-29 14:32:49 -0600

    4.1 (S) Server filters holdRoom actions through a store, and REST returns state of store, not hard-coded.

---
+++ server-store.js
@@ -0,0 +1,55 @@
+const { createStore } = require("redux");
+const initialState = {
+  rooms: [
+    { num: "30" },
+    { num: "31" },
+    { num: "20" },
+    { num: "21" },
+    { num: "10" },
+    { num: "11" }
+  ],
+  occupancy: {
+    "10": "full",
+    "11": "open",
+    "20": "open",
+    "21": "open",
+    "30": "open",
+    "31": "hold"
+  }
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // get all rooms at once, Promise-style
+    case "loadRooms":
+      return {
+        ...state,
+        room: action.payload
+      };
+    case "holdRoom":
+      const { hold } = action.payload;
+      return {
+        ...state,
+        occupancy: {
+          ...state.occupancy,
+          [action.payload.num]: hold ? "hold" : "open"
+        }
+      };
+    // get a single {num, occupancy} payload, where num is the key
+    case "setOccupancy":
+      const { occupancy, num } = action.payload;
+      return {
+        ...state,
+        occupancy: {
+          ...state.occupancy,
+          [num]: occupancy
+        }
+      };
+    default:
+      return state;
+  }
+};
+
+module.exports = {
+  store: createStore(reducer),
+  initialState: {}
+};
